b0c5fab17f5a7c37aca84a61b7324c43c1c59fd9
bahareh <bahareh.hassanvandi@gmail.com>
2026-06-12 12:45:46 +0330

    Add files via upload

---
+++ Dinners.py
@@ -0,0 +1,16 @@
+
+#دعوت دوستان به میز شام و جابجا کردن دو دوست
+
+dinner_1 = ['bahar' , 'yasna' , 'mahbobe']
+print("Can I invite you to dinner" + " " 'bahar')
+print("Can I invite you to dinner" + " " 'yasna')
+print("Can I invite you to dinner" + " " 'mahbobe')
+print("Mahbooba is not coming to my dinner invitation.")
+dinner_2 = dinner_1.pop(2)
+print(dinner_2)
+dinner_1.insert(2 ,'nazanin')
+print(dinner_1)
+print("Can I invite you to dinner" + " " 'bahar')
+print("Can I invite you to dinner" + " " 'yasna')
+print("Can I invite you to dinner" + " " 'nazanin')
+
